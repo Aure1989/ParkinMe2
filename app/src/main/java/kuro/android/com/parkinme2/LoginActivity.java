8026b26aa1520e14d0c6899395c462dc9b2b4b6f
Kuro89 <aurelio.dc.89@gmail.com>
2017-05-27 15:56:37 -0500

    Initial commit

---
+++ app/src/main/java/kuro/android/com/parkinme2/LoginActivity.java
@@ -0,0 +1,52 @@
+package kuro.android.com.parkinme2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        loginButton=(LoginButton)findViewById (R.id.fb_login_button);
+        textView=(TextView) findViewById(R.id.fb_login_status);
+        callbackManager= CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Sucess \n" +
+                        loginResult.getAccessToken().getUserId()+
+                        "\n"+loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                textView.setText("Login Error");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+}
